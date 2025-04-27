15017f345098ed788a0df82c0cd8fd6da3bf170d
pjpuzzler <pjpuzzler@gmail.com>
2025-04-27 16:29:15 -0400

    fix

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from utils import handle_new_posts
 
 if __name__ == "__main__":
     post_id = os.environ.get("POST_ID")
-    if post_id is not None:
+    if post_id:
         print(f'Got request to analyze post {post_id}')
         handle_new_posts(post_id)
     else:
