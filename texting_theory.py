eea8a2a0de9da9a7318a6a95ee079b94d705261d
pjpuzzler <pjpuzzler@gmail.com>
2025-04-29 20:49:41 -0400

    sizing

--- texting_theory.py
+++ texting_theory.py
@@ -162,7 +162,7 @@ def render_conversation(messages: list[TextMessage], color_data_left, color_data
     pad = 12 * scale
     line_sp = 6 * scale
     radius = 16 * scale
-    badge_sz = 36 * scale
+    badge_sz = 42 * scale
     badge_margin = 42 * scale
 
     max_bubble_w = int(img_w * 0.75)
