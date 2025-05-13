8db4f3078921109cbce1cbf8e620ec9aa4342eef
Patrick Johnson <pjpuzzler@gmail.com>
2025-05-13 19:59:36 -0400

    fix

--- random_key.py
+++ random_key.py
@@ -1,4 +1,4 @@
 import random
 
 def key_id():
-    return ["", "_ALT", "_ALT_2", "_ALT_3", "_ALT_4"][random.randint(4, 4)]
+    return ["", "_ALT", "_ALT_2", "_ALT_3", "_ALT_4"][random.randint(0, 0)]
