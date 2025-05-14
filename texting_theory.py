d70207b51fd669199b63b3f72f2f2711a3088355
Patrick Johnson <pjpuzzler@gmail.com>
2025-05-14 14:27:28 -0400

    fix

--- texting_theory.py
+++ texting_theory.py
@@ -48,7 +48,7 @@ class Classification(enum.Enum):
         return f"1024x/{self.value}_1024x.png"
 
 def api_key():
-    return os.environ["GEMINI_API_KEY" + key_id()]
+    return os.environ["GEMINI_API_KEY"]
 
 @dataclass
 class TextMessage:
